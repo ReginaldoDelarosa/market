d5e78b15c63d54d9164bcf897a79816efdc5cff8
ReginaldoDelarosa <rdelaros8@cuc.edu.co>
2023-10-06 18:56:36 -0500

    1

--- controllers/sales.controller.js
+++ controllers/sales.controller.js
@@ -1,56 +1,89 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'd1f4i7n8p4phd0xi',
-    password: 'ujr5diknro1d9q21',
-    database: 'ne70en5twe269oni',
-    port: 3306,
-    multipleStatements: true
-  });
+  host: "lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "d1f4i7n8p4phd0xi",
+  password: "ujr5diknro1d9q21",
+  database: "ne70en5twe269oni",
+  port: 3306,
+  multipleStatements: true,
+});
 
 exports.getSales = async (req, res) => {
   try {
     const conn = await pool.getConnection();
-    const [rows] = await conn.query('SELECT * FROM ventas');
+    const [rows] = await conn.query("SELECT * FROM ventas");
     conn.release();
     res.json(rows);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error al consultar las ventas');
+    res.status(500).send("Error al consultar las ventas");
   }
 };
+
 
 exports.createSale = async (req, res) => {
   try {
     const conn = await pool.getConnection();
-    const { codigo_producto, nombre_cliente, telefono_cliente, fecha_venta, cantidad_vendida, total_venta } = req.body;
-    const [result] = await conn.query('INSERT INTO ventas(codigo_producto, nombre_cliente, telefono_cliente, fecha_venta, cantidad_vendida, total_venta) VALUES (?, ?, ?, ?, ?, ?)', [codigo_producto, nombre_cliente, telefono_cliente, fecha_venta, cantidad_vendida, total_venta]);
-    const [rows] = await conn.query('SELECT * FROM productos WHERE codigo = ?', [codigo_producto]);
+    const {
+      codigo_producto,
+      nombre_cliente,
+      telefono_cliente,
+      fecha_venta,
+      cantidad_vendida,
+      total_venta,
+    } = req.body;
+    const [result] = await conn.query(
+      "INSERT INTO ventas(codigo_producto, nombre_cliente, telefono_cliente, fecha_venta, cantidad_vendida, total_venta) VALUES (?, ?, ?, ?, ?, ?)",
+      [
+        codigo_producto,
+        nombre_cliente,
+        telefono_cliente,
+        fecha_venta,
+        cantidad_vendida,
+        total_venta,
+      ]
+    );
+    const [rows] = await conn.query(
+      "SELECT * FROM productos WHERE codigo = ?",
+      [codigo_producto]
+    );
     const cantidadActual = rows[0].cantidad;
     const nuevaCantidad = cantidadActual - cantidad_vendida;
-    await conn.query('UPDATE productos SET cantidad = ? WHERE codigo = ?', [nuevaCantidad, codigo_producto]);
+    await conn.query("UPDATE productos SET cantidad = ? WHERE codigo = ?", [
+      nuevaCantidad,
+      codigo_producto,
+    ]);
     conn.release();
     res.json({ ...req.body, codigo: result.insertId });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error al crear la venta');
+    res.status(500).send("Error al crear la venta");
   }
 };
 
 exports.updateSale = async (req, res) => {
   try {
     const conn = await pool.getConnection();
-    const [rows] = await conn.query('SELECT * FROM ventas WHERE codigo = ?', [req.params.codigo]);
+    const [rows] = await conn.query("SELECT * FROM ventas WHERE codigo = ?", [
+      req.params.codigo,
+    ]);
     if (rows.length === 0) {
-      res.status(404).send('venta no encontrada');
+      res.status(404).send("venta no encontrada");
       return;
     }
     if (req.body.codigo) {
       res.status(400).send('No se puede actualizar el campo "codigo"');
       return;
     }
-    const { codigo_producto, nombre_cliente, telefono_cliente, fecha_venta, cantidad_vendida, total_venta } = req.body;
+    const {
+      codigo_producto,
+      nombre_cliente,
+      telefono_cliente,
+      fecha_venta,
+      cantidad_vendida,
+      total_venta,
+    } = req.body;
     const fieldsToUpdate = {};
     if (codigo_producto) fieldsToUpdate.codigo_producto = codigo_producto;
     if (nombre_cliente) fieldsToUpdate.nombre_cliente = nombre_cliente;
@@ -58,46 +91,53 @@ exports.updateSale = async (req, res) => {
     if (fecha_venta) fieldsToUpdate.fecha_venta = fecha_venta;
     if (cantidad_vendida) fieldsToUpdate.cantidad_vendida = cantidad_vendida;
     if (total_venta) fieldsToUpdate.total_venta = total_venta;
-    const [result] = await conn.query('UPDATE ventas SET ? WHERE codigo = ?', [fieldsToUpdate, req.params.codigo]);
+    const [result] = await conn.query("UPDATE ventas SET ? WHERE codigo = ?", [
+      fieldsToUpdate,
+      req.params.codigo,
+    ]);
     conn.release();
     if (result.affectedRows === 0) {
-      res.status(404).send('venta no encontrada');
+      res.status(404).send("venta no encontrada");
     } else {
       res.sendStatus(204);
     }
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error al actualizar la venta');
+    res.status(500).send("Error al actualizar la venta");
   }
 };
 exports.getSaleByCode = async (req, res) => {
   try {
     const conn = await pool.getConnection();
-    const [rows] = await conn.query('SELECT * FROM ventas WHERE codigo = ?', [req.params.codigo]);
+    const [rows] = await conn.query("SELECT * FROM ventas WHERE codigo = ?", [
+      req.params.codigo,
+    ]);
     conn.release();
     if (rows.length === 0) {
-      res.status(404).send('venta no encontrada');
+      res.status(404).send("venta no encontrada");
     } else {
       res.json(rows[0]);
     }
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error al obtener la venta');
+    res.status(500).send("Error al obtener la venta");
   }
 };
 
 exports.deleteSale = async (req, res) => {
   try {
     const conn = await pool.getConnection();
-    const [result] = await conn.query('DELETE FROM ventas WHERE codigo = ?', [req.params.codigo]);
+    const [result] = await conn.query("DELETE FROM ventas WHERE codigo = ?", [
+      req.params.codigo,
+    ]);
     conn.release();
     if (result.affectedRows === 0) {
-      res.status(404).send('venta no encontrada');
+      res.status(404).send("venta no encontrada");
     } else {
       res.sendStatus(204);
     }
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error al eliminar la venta');
+    res.status(500).send("Error al eliminar la venta");
   }
 };
